cdff4a648aeeb35716871b4673aa9bb904d8041d
abelucsd <65742650+abelucsd@users.noreply.github.com>
2025-08-25 17:18:45 -0400

    feat: update hero section.

--- frontend/src/pages/storefront/sections/Hero.tsx
+++ frontend/src/pages/storefront/sections/Hero.tsx
@@ -8,17 +8,17 @@ const Hero = () => {
             <div className="
                 order-2 lg:order-1 px-2 lg:px-0 flex flex-col gap-6 flex-1 xl:flex-2
             ">
-                <h1>DEVELOPING & PROVIDING INNOVATIVE CONSTRUCTION SOLUTIONS</h1>
+                <h1>SMALL BUSINESS PHRASE</h1>
                 <button className="btn-primary w-[300px]">
-                    <h5>Request an Appointment</h5>
+                    <h5>Browse Products</h5>
                 </button>
             </div>
             <div className="lg:order-2 w-screen relative flex-2">
                 <img className="
                     w-[600px] h-[400px] 
                     md:w-[1000px] md:h-[600px]
-                    lg:rounded-4xl" 
-                    src={'images/construction/construction-1.jpg'} 
+                    lg:rounded-4xl"
+                    src={''} 
                 />
             </div>
         </section>
