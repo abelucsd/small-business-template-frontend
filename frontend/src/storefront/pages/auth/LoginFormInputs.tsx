e145dcb03a4753af273efd6be10165ae9000bf79
abelucsd <65742650+abelucsd@users.noreply.github.com>
2025-09-11 11:10:11 -0400

    styles: add background-color for authentication page, and update login form margins to the middle.

--- frontend/src/storefront/pages/auth/LoginFormInputs.tsx
+++ frontend/src/storefront/pages/auth/LoginFormInputs.tsx
@@ -15,7 +15,7 @@ const LoginFormInputs = ({register, errors}: LoginFormInputs) => {
               type="email" 
               {...register("email", { required: "Email is required" })}
               required            
-              className="block border rounded-md w-full md:w-[300px] grow mt-4 py-2 pr-3 pl-1 textr-black placeholder:text-gray-500 focus:outline-none sm:text-sm/6 "
+              className="block border rounded-md w-full md:w-[400px] grow mt-4 py-2 pr-3 pl-1 textr-black placeholder:text-gray-500 focus:outline-none sm:text-sm/6 "
           />
           {errors.email && (
             <p>{errors.email.message}</p>
@@ -31,7 +31,7 @@ const LoginFormInputs = ({register, errors}: LoginFormInputs) => {
               minLength: { value: 6, message: "Min length is 6" },
             })}
             required
-            className="block border rounded-md w-full md:w-[300px] grow mt-4 py-2 pr-3 pl-1 textr-black placeholder:text-gray-500 focus:outline-none sm:text-sm/6 "
+            className="block border rounded-md w-full md:w-[400px] grow mt-4 py-2 pr-3 pl-1 textr-black placeholder:text-gray-500 focus:outline-none sm:text-sm/6 "
           />
           {errors.password && (
             <p>{errors.password.message}</p>
