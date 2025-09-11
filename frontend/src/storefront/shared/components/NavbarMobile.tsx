7313d0068a447c1df7f783304be1731b7a376784
abelucsd <65742650+abelucsd@users.noreply.github.com>
2025-09-11 13:33:36 -0400

    feat: implement mobile navbar.

--- frontend/src/storefront/shared/components/NavbarMobile.tsx
+++ frontend/src/storefront/shared/components/NavbarMobile.tsx
@@ -1,15 +1,55 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { navList } from '../../utils/data';
+import { useCategories } from '../api/useCategories';
+import { Link, useNavigate } from 'react-router-dom';
 
-const NavbarMobile = () => {
+interface NavbarMobileProps {
+    styles?: string;
+    onLinkClick: () => void;
+}
+
+const NavbarMobile = ({styles, onLinkClick}: NavbarMobileProps) => {
+    const {
+        categories,
+        isLoading,
+        isError,
+    } = useCategories();
+
+    const navigate = useNavigate();
+    const handleProductClick = (name: string) => {
+        // Navigate to ProductDetail page
+        navigate(`/Products/category/${name}`);
+    };
+
+    if (isError || isLoading) {
+        return (
+        <div className="container mx-auto flex flex-row justify-between">
+            <p>Loading...</p>
+        </div>
+        );
+    };
+
+
     return (
-        <ul className='
-            absolute flex flex-col gap-10 mt-100 px-8 py-10 bg-[var(--bg-nav-dropdown)] w-8/10
-            left-1/2 transform -translate-x-1/2 rounded-xl text-white
-            '>
-            {navList.map((item, index) => 
-                <h5 key={index}>{item.name}</h5>
+        <ul className={`
+            ${styles}            
+            p-4 bg-white text-black
+            flex flex-col gap-12
+            `}>
+            <h3>Categories</h3>
+            {categories.map((item, index) => 
+                <h5 key={index} onClick={() => {
+                    handleProductClick(item.name);
+                    onLinkClick();
+                }}>{item.name}</h5>
             )}
+            <hr className='w-1/3'/>
+            {navList.map((item, index) => 
+                <Link key={index} to={item.link} onClick={onLinkClick}>
+                    <h5>{item.name}</h5>
+                </Link>
+            )}
+
         </ul>
     );
 };
