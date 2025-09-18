49a693295f4b83450281ec670ec6de42a91c132c
abelucsd <65742650+abelucsd@users.noreply.github.com>
2025-09-18 12:42:50 -0400

    feat: implement CartProvider.

---
+++ frontend/src/storefront/pages/cart/CartProvider.tsx
@@ -0,0 +1,78 @@
+import React, { createContext, useContext, useState } from 'react';
+import type { Product } from '../products/types';
+
+
+type CartContextType = {
+  items: {[id: string]: {item: Product, quantity: number}},
+  addItem: (item: Product, quantity: number) => void;
+  totalUniqueItems: () => void;
+  isEmpty: () => void;
+  updateItemQuantity: (id: string, quantity: number) => void;
+  removeItem: (id: string) => void;
+};
+
+
+const CartContext = createContext<CartContextType | undefined>(undefined);
+
+
+export const CartProvider = ({children}: {children: React.ReactNode}) => {
+  const [items, setItems] = useState<{[id: string]: {item: Product, quantity: number}}>({});
+
+  const addItem = (item: Product, quantity: number) => {
+    try {
+      setItems(prev => ({
+        ...prev,
+        [item.id]: { item: item, quantity: quantity },
+      }));
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const removeItem = (id: string) => {
+    try {
+      const newItems = { ...items };
+      if (newItems[id] === undefined) {
+        throw new Error(`Item with id ${id} does not exist in the cart.`)
+      }
+      delete newItems[id]
+      setItems(newItems);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  
+  const isEmpty = () => Object.keys(items).length === 0;    
+
+  const totalUniqueItems = () => Object.keys(items).length;
+
+  const updateItemQuantity = (id: string, quantity: number) => {
+    try {
+      setItems( prev => {
+        if (!prev[id]) {
+          throw new Error(`Cannot update quantity: item with id ${id} not found in the cart.`)        
+        }
+        return {
+          ...prev, 
+          [id]: { ...prev[id], quantity }
+        };
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };  
+
+
+  return (
+    <CartContext.Provider value={{items, addItem, removeItem, isEmpty, totalUniqueItems, updateItemQuantity}}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+
+export const useCartContext = () => {
+  const ctx = useContext(CartContext);
+  if (!ctx) throw new Error('useCartContext must be used within a CartProvider')
+  return ctx;
+};
