3e021afef5b8aee62b8d59a1f92360b160d0bc51
abelucsd <65742650+abelucsd@users.noreply.github.com>
2025-09-19 15:56:22 -0400

    feat: implement cart and checkout workflow
    * Includes:
      - Implement Cart and Checkout workflow.

--- frontend/src/storefront/pages/products/productDetail/Order.tsx
+++ frontend/src/storefront/pages/products/productDetail/Order.tsx
@@ -1,16 +1,17 @@
+import { useCartContext } from "../../cart/CartProvider";
 import type { Product } from "../types";
 
 interface ProductOrderProps {
   product: Product;
-  onAddToCart: (product: Product) => void;
   styles?: string;
 }
 
-const ProductOrder = ({product, onAddToCart, styles}: ProductOrderProps) => {
+const ProductOrder = ({product, styles}: ProductOrderProps) => {  
+  const {addItem} = useCartContext();
   return (
     <div>
       <button
-        onClick={() => onAddToCart?.(product)}
+        onClick={() => addItem(product, 1)}
         className={`${styles} bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700`}
       >
         Add to Cart
--- frontend/src/storefront/utils/data.ts
+++ frontend/src/storefront/utils/data.ts
@@ -1,5 +1,6 @@
-export const API_URL = import.meta.env.VITE_API_URL;
-// export const API_URL = 'http://localhost:4000/';
+
+// export const API_URL = import.meta.env.VITE_API_URL;
+export const API_URL = 'http://localhost:4000/';
 
 export { default as heroBannerImg }  from '../../assets/images/hero-banner-ecommerce.png';
 
@@ -7,5 +8,5 @@ export const navList = [
     {name: "Products", link:"/Products"},
     {name: "About", link:"/About"},
     {name: "Contact", link:"/Contact"},
-    {name: "Login", link:"/Login"},
+    {name: "Login", link:"/Login"},    
 ];
