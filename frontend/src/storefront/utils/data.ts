cc71423fcfdcf7b277e950b275cab76db088de05
abelucsd <65742650+abelucsd@users.noreply.github.com>
2025-09-19 16:35:23 -0400

    chore: ready for deployment.

--- frontend/src/storefront/utils/data.ts
+++ frontend/src/storefront/utils/data.ts
@@ -1,6 +1,6 @@
 
-// export const API_URL = import.meta.env.VITE_API_URL;
-export const API_URL = 'http://localhost:4000/';
+export const API_URL = import.meta.env.VITE_API_URL;
+// export const API_URL = 'http://localhost:4000/';
 
 export { default as heroBannerImg }  from '../../assets/images/hero-banner-ecommerce.png';
 export { default as saleBannerImg } from '../../assets/images/sale-banner-ecommerce.png';
