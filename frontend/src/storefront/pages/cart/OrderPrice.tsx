a6c52a83fe60ee9d21f3a22ee592312a5acff925
abelucsd <65742650+abelucsd@users.noreply.github.com>
2025-09-20 13:25:31 -0400

    release/v1.0.7
    * Includes:
      - Add on sale banner.
      - Integrate navigation on storefront buttons and banners.

--- frontend/src/storefront/pages/cart/OrderPrice.tsx
+++ frontend/src/storefront/pages/cart/OrderPrice.tsx
@@ -7,14 +7,14 @@ interface OrderPriceProps {
   shipping: number;
 }
 
-const OrderPrice = ({totalItems, totalItemsPrice, tax, shipping}: OrderPriceProps) => {
+const OrderPrice = ({totalItems, totalItemsPrice, tax, shipping}: OrderPriceProps) => {  
 
   return (
     <div className="w-2/5">
       <div className="flex flex-row justify-between"><p>Items ({totalItems}): </p><p>${totalItemsPrice}</p></div>        
       <div className="flex flex-row justify-between"><p>Shipping & handling: </p><p>${shipping}</p></div>
       <div className="flex flex-row justify-between"><p>Estimated tax to be collected: </p><p>${tax}</p></div>
-      <div className="flex flex-row justify-between"><h2>Order Total: </h2><h2>${totalItemsPrice + shipping + tax}</h2></div>
+      <div className="flex flex-row justify-between"><h2>Order Total: </h2><h2>${Number((totalItemsPrice + shipping + tax).toFixed(2))}</h2></div>
     </div>
   )
 }
