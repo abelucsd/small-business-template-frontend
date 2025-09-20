519078b32609db7297db096f200e549d021effb7
abelucsd <65742650+abelucsd@users.noreply.github.com>
2025-09-20 13:05:37 -0400

    release/v1.0.7
    * Includes:
      - Implement CustomModal.tsx.
      - Integrate CustomModal in ProductDetail and Cart.

---
+++ frontend/src/storefront/shared/components/Modal.tsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import Modal from "react-modal";
+
+const modalStyles = {
+  content: {            
+    height: '160px',
+    top: '30%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',        
+    marginRight: '-50%',    
+    transform: 'translate(-50%, -50%)',
+  },
+};
+
+
+Modal.setAppElement('#root');
+
+interface CustomModalProps {
+
+  header: string;
+  onResult: (result: boolean) => void;
+};
+
+const CustomModal = ({ header, onResult}: CustomModalProps) => {
+  const [modalIsOpen, setIsOpen] = useState<boolean>(true);
+
+  const closeModal = (result: boolean) => {
+    onResult(result);
+    setIsOpen(false);    
+  }
+
+
+  return (
+    <Modal
+      isOpen={modalIsOpen}      
+      style={modalStyles}        
+    >       
+      <div className="flex flex-col justify-between h-full py-2 px-2">
+        <h3>{header}</h3>
+        <div className="flex flex-row gap-4 justify-end">
+          <button onClick={() => {            
+            closeModal(true);
+          }}
+          className="border hover:bg-gray-100 px-4 py-1"
+          >
+            Yes
+          </button>
+          <button onClick={() => closeModal(false)} className="border hover:bg-gray-100 px-4 py-1">
+            No
+          </button>
+        </div>
+      </div>
+    </Modal>
+
+  )
+}
+
+export default CustomModal;
